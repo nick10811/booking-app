16fadd74355a38d08a7fa8fd6574876a10ee3e95
Nick Yang <nick10811@gmail.com>
2022-11-13 20:53:48 +0800

    Allow users to repeat booking.

--- main.go
+++ main.go
@@ -21,41 +21,45 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	// Go is a statically typed language
-	// You need to tell Go Compiler, the data type when declaring the variable.
-	var userName string
-	var userEmail string
-	var userTickets uint
+	// infinite loop: interrupted by ctrl+c
+	// allow users to repeat booking
+	for {
+		// Go is a statically typed language
+		// You need to tell Go Compiler, the data type when declaring the variable.
+		var userName string
+		var userEmail string
+		var userTickets uint
 
-	// ask user for their name
-	fmt.Print("Enter your name: ")
-	fmt.Scan(&userName)
-	// userName = "Tom"
-	fmt.Print("Enter your email address: ")
-	fmt.Scan(&userEmail)
-	fmt.Print("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
-	// userTickets = 2
+		// ask user for their name
+		fmt.Print("Enter your name: ")
+		fmt.Scan(&userName)
+		// userName = "Tom"
+		fmt.Print("Enter your email address: ")
+		fmt.Scan(&userEmail)
+		fmt.Print("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+		// userTickets = 2
 
-	// remainingTickets = remainingTickets - uint(userTickets)
-	remainingTickets -= userTickets
-	bookings = append(bookings, userName)
+		// remainingTickets = remainingTickets - uint(userTickets)
+		remainingTickets -= userTickets
+		bookings = append(bookings, userName)
 
-	// fmt.Println(remainingTickets)  // print value of remainingTickets
-	// fmt.Println(&remainingTickets) // print memory address of remainingTickets
+		// fmt.Println(remainingTickets)  // print value of remainingTickets
+		// fmt.Println(&remainingTickets) // print memory address of remainingTickets
 
-	// fmt.Printf("The whole array: %v\n", bookings)   // [Nick ]
-	// fmt.Printf("The first value: %v\n", bookings[0])
-	// fmt.Printf("Array type: %T\n", bookings)        // [50]string
-	// fmt.Printf("Array length: %d\n", len(bookings)) // 50
-	// fmt.Printf("The whole slice: %v\n", bookings)   // [Nick]
-	// fmt.Printf("The first value: %v\n", bookings[0])
-	// fmt.Printf("Slice type: %T\n", bookings)        // []string
-	// fmt.Printf("Slice length: %d\n", len(bookings)) // 1
+		// fmt.Printf("The whole array: %v\n", bookings)   // [Nick ]
+		// fmt.Printf("The first value: %v\n", bookings[0])
+		// fmt.Printf("Array type: %T\n", bookings)        // [50]string
+		// fmt.Printf("Array length: %d\n", len(bookings)) // 50
+		// fmt.Printf("The whole slice: %v\n", bookings)   // [Nick]
+		// fmt.Printf("The first value: %v\n", bookings[0])
+		// fmt.Printf("Slice type: %T\n", bookings)        // []string
+		// fmt.Printf("Slice length: %d\n", len(bookings)) // 1
 
-	fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email at %v\n", userName, userTickets, userEmail)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email at %v\n", userName, userTickets, userEmail)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
 
 }
