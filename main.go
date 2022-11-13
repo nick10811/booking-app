fb1eab904505b0257f8477744efe4476dede3747
Nick Yang <nick10811@gmail.com>
2022-11-13 21:35:29 +0800

    Validate user input.

--- main.go
+++ main.go
@@ -34,56 +34,63 @@ func main() {
 		var userEmail string
 		var userTickets uint
 
-		// ask user for their name
 		fmt.Print("Enter your first name: ")
 		fmt.Scan(&firstName)
+
 		fmt.Print("Enter your last name: ")
 		fmt.Scan(&lastName)
-		// userName = "Tom"
+
 		fmt.Print("Enter your email address: ")
 		fmt.Scan(&userEmail)
+
 		fmt.Print("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
-		// userTickets = 2
 
-		if userTickets > remainingTickets {
-			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
-			continue
-		}
+		// validate user input
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(userEmail, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets // positive number + greater than 0
 
-		// remainingTickets = remainingTickets - uint(userTickets)
-		remainingTickets -= userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		if isValidName && isValidEmail && isValidTicketNumber {
+			// remainingTickets = remainingTickets - uint(userTickets)
+			remainingTickets -= userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		// fmt.Println(remainingTickets)  // print value of remainingTickets
-		// fmt.Println(&remainingTickets) // print memory address of remainingTickets
+			// fmt.Println(remainingTickets)  // print value of remainingTickets
+			// fmt.Println(&remainingTickets) // print memory address of remainingTickets
 
-		// fmt.Printf("The whole array: %v\n", bookings)   // [Nick ]
-		// fmt.Printf("The first value: %v\n", bookings[0])
-		// fmt.Printf("Array type: %T\n", bookings)        // [50]string
-		// fmt.Printf("Array length: %d\n", len(bookings)) // 50
-		// fmt.Printf("The whole slice: %v\n", bookings)   // [Nick]
-		// fmt.Printf("The first value: %v\n", bookings[0])
-		// fmt.Printf("Slice type: %T\n", bookings)        // []string
-		// fmt.Printf("Slice length: %d\n", len(bookings)) // 1
+			// fmt.Printf("The whole array: %v\n", bookings)   // [Nick ]
+			// fmt.Printf("The first value: %v\n", bookings[0])
+			// fmt.Printf("Array type: %T\n", bookings)        // [50]string
+			// fmt.Printf("Array length: %d\n", len(bookings)) // 50
+			// fmt.Printf("The whole slice: %v\n", bookings)   // [Nick]
+			// fmt.Printf("The first value: %v\n", bookings[0])
+			// fmt.Printf("Slice type: %T\n", bookings)        // []string
+			// fmt.Printf("Slice length: %d\n", len(bookings)) // 1
 
-		fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, userTickets, userEmail)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+			fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, userTickets, userEmail)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-		firstNames := []string{}
-		// for-each
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+			firstNames := []string{}
+			// for-each
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
-		// boolean
-		var noTicketsRemaning bool = remainingTickets == 0
-		if noTicketsRemaning {
-			// end program
-			fmt.Println("Our conference is booked out. Come back next year.")
-			break
+			// boolean
+			var noTicketsRemaning bool = remainingTickets == 0
+			if noTicketsRemaning {
+				// end program
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+
+		} else {
+			fmt.Println("Your input data is invalid, try again")
+			// fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			// continue
 		}
 	}
 
